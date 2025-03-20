f9f023e7388764e4c781fc67d096f00eb41f1abe
ThwyIgo <thiagopachecorocha@hotmail.com>
2025-03-19 23:45:48 -0300

    Initial commit

---
+++ .emacs.el
@@ -0,0 +1,53 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(eval-when-compile (require 'use-package))
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+(when (not package-archive-contents)
+    (package-refresh-contents))
+
+(use-package auctex
+  :custom
+  (TeX-auto-save t)
+  (TeX-parse-self t)
+  (LaTeX-electric-left-right-brace t)
+  :hook
+  (TeX-mode . prettify-symbols-mode)
+  (LaTeX-mode . (lambda ()
+                  (push (list 'output-pdf "Zathura")
+                        TeX-view-program-selection)))
+  (doc-view-mode . (lambda () (display-line-numbers-mode -1)))
+  )
+
+(use-package yasnippet
+  :config
+  (yas-global-mode 1)
+  (use-package yasnippet-snippets))
+
+;; Spell check a single word or region: M-$
+;; Change the language of a single buffer: M-x ispell-change-dictionary
+;(setq-default ispell-aspell-dict-dir nil) ;; Uncomment if aspell dict dir breaks and restart emacs
+(use-package ispell
+  :custom
+  (ispell-program-name "aspell")
+  (ispell-dictionary "english") ; brasileiro for pt-BR
+  (ispell-dictionary-alist (ispell-find-aspell-dictionaries))
+  )
+(use-package flyspell
+  :hook
+  (org-mode . flyspell-mode)
+  (text-mode . (lambda () ;; Don't start flyspell in the scratch buffer
+                 (unless (string= (buffer-name) "*scratch*") (flyspell-mode))
+                 ))
+  )
+
+(use-package which-key
+  :custom
+  (which-key-idle-delay 2.0)
+  :config
+  (which-key-mode)
+  )
+
+(use-package symbol-overlay
+  :hook (prog-mode . symbol-overlay-mode))
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "TCC em LaTeX com Emacs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-basic
+          # Pacotes mínimos para C-c C-p C-b funcionar
+          memoir xpatch booktabs textcase enumitem
+          # Pacotes para fazer latexmk funcionar
+          breakurl xkeyval
+          babel-portuges
+          latexmk
+          microtype
+          abntex2
+        ;
+      };
+    in {
+      #packages.default = {};
+
+      devShell = pkgs.mkShell {
+        packages = [
+          tex
+          pkgs.ghostscript_headless
+
+          pkgs.poppler_utils # Procurar por texto no Preview-pane do Emacs
+
+          pkgs.zathura
+          (pkgs.aspellWithDicts (dicts: with dicts; [ en pt_BR ]))
+          pkgs.emacs
+        ];
+
+        shellHook = ''
+          alias build="latexmk -auxdir=./out"
+          alias preview="latexmk -auxdir=./out -pdf -pvc"
+          alias edit="emacs -l ${./.emacs.el}"
+        '';
+      };
+    });
+}
